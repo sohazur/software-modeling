fae69dbd6ce06107eb0de8e97b969b0a2befd050
Sohazur <mdsohazurislam@gmail.com>
2023-03-05 11:10:10 +0400

    Added year attribute to Vehicle class

--- Vehicle.py
+++ Vehicle.py
@@ -1,9 +1,10 @@
 class Vehicle:
-    def __init__(self, vehicleId, make, model, color):
+    def __init__(self, vehicleId, make, model, color, year):
         self.__vehicleId = vehicleId
         self.__make = make
         self.__model = model
         self.__color = color
+        self.__year = year
 
     def getVehicleId(self):
         return self.__vehicleId
@@ -25,3 +26,9 @@ class Vehicle:
 
     def setColor(self, color):
         self.__color = color
+        
+    def getYear(self):
+        return self.__year
+    
+    def setYear(self, year):
+        self.__year = year
